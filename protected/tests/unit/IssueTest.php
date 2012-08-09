93181003707e9baa0ad6dd0025366cb8253ed284
Zhao Rui <zratcg@gmail.com>
2012-08-09 17:19:12 +0800

    p119

---
+++ protected/tests/unit/IssueTest.php
@@ -0,0 +1,11 @@
+<?php
+  class ProjectTest extends CDbTestCase {
+    public function testGetTypes() {
+      $options = Issue::model()->typeOptions;
+      $this->assertTrue(is_array($options));
+      $this->assertTrue(3 == count($options));
+      $this->assertTrue(in_array('Bug', $options));
+      $this->assertTrue(in_array('Feature', $options));
+      $this->assertTrue(in_array('Task', $options));
+    }
+  }
